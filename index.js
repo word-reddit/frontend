ebbe1e573b5142310af09b03e23f9273ff9ab70f
garned <tobias.keel@bluewin.ch>
2017-07-21 16:07:14 +0200

    refactoring/minor restructuring
    refactoring/minor restructuring
    click listeners for posts and their subreddits added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = 80;
+var port = 8000; //use port >1024 so it doesnt need sudo to run on mac and linux
 
 var publicPath = path.resolve(__dirname, 'www');
 app.use(express.static('www'));
--- www/js/gui.js
+++ www/js/gui.js
@@ -1,41 +1,90 @@
 var server = "http://localhost:8080";
+var currentSub = "popular";
+var currentSubPage = "";
+
+var subredditTempStr;
+var postTempStr;
+var container;
 
 $( document ).ready(function(){
-  getSubReddit("popular", "");
-  initActionListeners();
+  subredditTempStr = $("#post-template").html();
+  postTempStr = $("#single-post-template").html();
+  container = $("#sheet-content");
+
+  getSubReddit(currentSub, currentSubPage);
 });
 
-function initActionListeners() {}
+function initActionListeners() {
+  $(".post-title").click(function(event){
+    getPost($(event.target).attr('permalink'));
+  });
+
+  $(".subName").click(function(event){
+    console.log('click on subreddit Name: ');
+    getSubReddit($(event.target).attr('subreddit'), '');
+  });
+
+}
 
 
 function getSubReddit(subName, pageName) {
-  makeRequest("/r/" + subName + "/" + pageName);
+  currentSub = subName;
+  makeRequest("/r/" + subName + "/" + pageName, "subreddit");
 }
+
+
+function getPost(permalink){
+  currentSub = permalink.split("/")[2];
+  makeRequest(permalink, "post");
+}
+
+
 
 function renderSubReddit(data){
-  var templateStr = $("#post-template").html();
-  var container = $("#sheet");
-  //$("#subredditName")
+  container.html("");
+  $("#subredditName").html(currentSub);
 
   data.data.children.forEach(function(item, index){
-    var html = Mustache.to_html(templateStr, item.data);
+    var html = Mustache.to_html(subredditTempStr, item.data);
     container.append(html);
   });
+
+  initActionListeners();
 }
 
-function makeRequest(url) {
+function renderPost(data){
+  container.html("");
+  $("#subredditName").html(currentSub);
+}
+
+function makeRequest(url, dataType) {
   var address = server + url;
+
+  console.log(address);
 
   $.ajax({
     url: address,
     async: true,
     dataType: "jsonp", //Use JSONP, the browser will block the cross-domain request otherwise
-    success: function success(data) {
-      renderSubReddit(data);
+    success: function(data) {
+      switch(dataType){
+        case("subreddit"):
+          renderSubReddit(data);
+          break;
+        case("post"):
+          renderPost(data);
+          break;
+        default:
+          console.log("invalid dataType in makeRequest()");
+          break;    
+      }
     },
-    error: function error(_error) {
+    error: function(_error) {
       console.log(_error.statusCode());
       return { error: "oh-shit" };
     }
   });
 }
+
+
+
